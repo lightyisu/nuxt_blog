cd4746dc203352801642739689d250ca4d884668
lightyisu <1513221978@qq.com>
2022-05-08 16:45:06 +0800

    每次部署清除缓存

--- gulpfile.js
+++ gulpfile.js
@@ -1,7 +1,25 @@
 const axios = require("axios");
 const fs = require("fs");
+function delDir(path){
+  let files = [];
+  if(fs.existsSync(path)){
+      files = fs.readdirSync(path);
+      files.forEach((file, index) => {
+          let curPath = path + "/" + file;
+          if(fs.statSync(curPath).isDirectory()){
+              delDir(curPath); //递归删除文件夹
+          } else {
+              fs.unlinkSync(curPath); //删除文件
+          }
+      });
+    
+  }
+}
+
+
 
 async function getSeverData() {
+  delDir("content/articles")
   let { data } = await axios.get("https://asop2rq5.directus.app/items/article");
   console.log("向服务器请求post");
   data.data.forEach(async (item) => {
